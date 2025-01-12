435aacdd2e469d911d1c1456297d5db67ae72369
armond <armondscomputer@armonds-MacBook-Pro.local>
2025-01-12 15:51:01 -0500

    Enough to pass, gonna try to complete everything before being totally done

--- aa08-assessment-version-a-web-traffic-ft-main/json-api-server/server/server.js
+++ aa08-assessment-version-a-web-traffic-ft-main/json-api-server/server/server.js
@@ -282,6 +282,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 server.listen(port, () => console.log('Server is listening on port', port));
